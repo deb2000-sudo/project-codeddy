035d4f23fce1be7dfb865a188b6618b017e4d825
snehaM2210 <snehamallik665@gmail.com>
2021-04-29 14:29:12 +0530

    modified webpage

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Editor = () => {
+    return(
+        <div className="container">
+            <div className="header">
+                <h1>CodEddy</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Editor;
