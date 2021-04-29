ddd6fbfdc8e1f5893f1c5989867cbe4c0b7599cc
snehaM2210 <snehamallik665@gmail.com>
2021-04-29 15:18:32 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Editor = () => {
-    return(
+class Editor extends Component {
+  render() {
+    return (
         <div className="container">
             <div className="header">
                 <h1>CodEddy</h1>
             </div>
+            <div className="input-group">
+            <textarea 
+                className="text-box" 
+                id="text1"
+                rows="30"
+                cols="100">
+            </textarea>
+        </div>
         </div>
     );
-};
+    }
+}
 
 export default Editor;
